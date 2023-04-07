475cdcb18b5386c2950b1003ac01d440dfbc38e1
Ashlyn Smith <SMITHASHLYN210@YAHOO.COM>
2023-04-06 23:22:58 -0500

    JavaScript and ReadMe updated

--- script.js
+++ script.js
@@ -32,9 +32,9 @@ let password = ""
 let passwordLength =prompt ("how long do you want the password")
 for (let i = 0; i < passwordLength; i++) {
 
-  Math.floor(Math.random() * builtArray.length);
+  let generatedLetterIndex = Math.floor(Math.random() * builtArray.length);
 
-    password += ""
+    password += builtArray[generatedLetterIndex]
 
 }
    
