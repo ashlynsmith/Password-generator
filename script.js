89c802e34c796949b75fa6dde6eb3a2755f116d5
Ashlyn Smith <SMITHASHLYN210@YAHOO.COM>
2023-04-07 20:45:23 -0500

    changes added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+
 let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -17,40 +17,46 @@ function generatePassword(){
         builtArray = [...builtArray, ...lowerAlphabet];
 
     }
+  
     if (confirm("Do you want your password to contain uppercase letters?")){
         builtArray = [...builtArray, ...upperAlphabet]
     }
+   
+
     if(confirm("Do you want your password to contain numbers?")){
         builtArray = [...builtArray, ...numbers]
     }
+    
+    if(builtArray.length === 0){
+        alert("Please enter at least one option")
+        generatePassword()
+    }
+    
+    let password = ""
+
+    // password no less than 8 characters and no greater than 128 characters
+
+    let passwordLength = prompt("How long would you like your password to be?")
+    if (passwordLength < 8 ) {
+        alert("Must be greater than 8")
+        return false
+    }
+    if (passwordLength > 128){
+        alert("Must be less than 128")
+        return false
+    }
+
+    for (let i = 0; i < passwordLength; i++) {
+
+    let generatedLetterIndex = Math.floor(Math.random() * builtArray.length);
+
+        password += builtArray[generatedLetterIndex]
+
+    }
+    
 
 
-
-let password = ""
-
-// password no less than 8 characters and no greater than 128 characters
-
-let passwordLength = prompt("How long would you like your password to be?")
-if (passwordLength < 8 ) {
-    alert("Must be greater than 8")
-    return
-}
-if (passwordLength > 128){
-    alert("Must be less than 128")
-    return
-}
-
-for (let i = 0; i < passwordLength; i++) {
-
-  let generatedLetterIndex = Math.floor(Math.random() * builtArray.length);
-
-    password += builtArray[generatedLetterIndex]
-
-}
-   
-
-
-//string based off of randomly selected values from an array
+    //string based off of randomly selected values from an array
 
 
     return password;
